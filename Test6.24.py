e694858730215ca1f20d876ac312ed5a2337c67e
zhanglu1227 <dmaozl@sina.com>
2019-06-24 23:48:38 +0800

    每日编程题

---
+++ Test6.24.py
@@ -0,0 +1,19 @@
+'''
+给定一个整数数组nums和一个目标值target,请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标
+给定nums = [2, 7, 11, 15]
+target = 9
+'''
+
+
+def sumA(nums,target):
+
+    a = len(nums)
+    for i in range(a):
+        for j in range(i, a):
+            if nums[i] + nums[j] == target:
+                return (i,j)
+
+nums = [2, 7, 11, 15]
+target = 9
+b = sumA(nums, target)
+print(b)
